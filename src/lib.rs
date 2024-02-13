f39df2b6ee30bf6af887f8a6003cc3f408b3c0a3
Marc Beck König <mki@skytem.com>
2024-02-13 09:00:03 +0100

    add library target

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod commands;
+pub mod config;
+pub mod err_msg_parse;
+pub mod errlog;
+pub mod gh;
+pub mod issue;
+pub mod util;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 use std::error::Error;
 
-mod commands;
-pub mod config;
-pub mod err_msg_parse;
-pub mod errlog;
-pub mod gh;
-pub mod issue;
-pub mod util;
+use gh_workflow_parser::{commands, config};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let config = config::init()?;
