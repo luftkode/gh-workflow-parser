519c7eafcec72a0f4ee507528e8b0ab34098c722
Marc Beck König <mki@skytem.com>
2024-02-13 15:41:02 +0100

    remove unused

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 use std::error::Error;
-use std::fs::File;
-use std::io::{self, Read};
+use std::io::Read;
 use std::{env, fs, path::Path};
 
 // Convenience macro for printing warnings during the build process
@@ -22,18 +21,6 @@ const GH_CLI_COMPRESSED_PATH: &str = "gh_cli/compressed/gh_cli_bz2";
 
 /// Name of the file that contains the byte array for the GitHub CLI file
 const INCLUDE_GH_CLI_FILE: &str = "include_gh_cli.rs";
-
-/// Get the length (size in bytes) of a file
-///
-/// Reads the whole file into memory and returns the length of the vector.
-/// This is more reliable than using `std::fs::metadata` because it doesn't rely on the file system.
-pub fn file_len(fpath: &Path) -> io::Result<usize> {
-    const PRE_ALLOC: usize = 1024 * 1024 * 20; // 20 MiB
-    let mut file = File::open(fpath)?;
-    let mut raw_mtgogetter = Vec::with_capacity(PRE_ALLOC);
-    file.read_to_end(raw_mtgogetter.as_mut())?;
-    Ok(raw_mtgogetter.len())
-}
 
 /// Compression for the GitHub CLI file (set the compression even higher if the file size is too large for crates.io)
 pub fn bzip2_compress(input: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
@@ -69,6 +56,10 @@ fn main() {
 fn include_gh_cli(out_dir: &Path) -> Result<(), Box<dyn Error>> {
     let gh_cli_bytes = fs::read(GH_CLI_COMPRESSED_PATH).expect("Failed to read gh_cli/gh");
     let gh_cli_size = gh_cli_bytes.len();
+    assert!(
+        gh_cli_size < MAX_CRATES_IO_UPLOAD_SIZE,
+        "gh_cli_bz2 size exceeds maximum size for crates.io"
+    );
 
     let gh_cli_path = out_dir.join("gh_cli_bz2");
     fs::write(&gh_cli_path, gh_cli_bytes).expect("Failed to write gh_cli_bz2");
