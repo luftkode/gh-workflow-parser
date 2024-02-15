7330745932060a10d31534b6bc93f180c897e105
Marc Beck König <mki@skytem.com>
2024-02-15 10:47:09 +0100

    refactor

--- src/err_msg_parse/yocto_err.rs
+++ src/err_msg_parse/yocto_err.rs
@@ -1,5 +1,6 @@
 use crate::{err_msg_parse::LOGFILE_MAX_LEN, util::first_abs_path_from_str};
-use std::{error::Error, fmt::Display};
+use std::error::Error;
+use strum::*;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct YoctoError {
@@ -34,114 +35,125 @@ pub struct YoctoFailureLog {
     pub contents: String,
 }
 
-#[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord, Copy)]
+#[derive(
+    Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord, Copy, Display, EnumString, EnumIter,
+)]
 pub enum YoctoFailureKind {
     /// The 6 standard tasks in Yocto https://docs.yoctoproject.org/ref-manual/tasks.html
+    #[strum(serialize = "do_build")]
     DoBuild,
+    #[strum(serialize = "do_compile")]
     DoCompile,
+    #[strum(serialize = "do_compile_ptest_base")]
     DoCompilePtestBase,
+    #[strum(serialize = "do_configure")]
     DoConfigure,
+    #[strum(serialize = "do_configure_ptest_base")]
     DoConfigurePtestBase,
+    #[strum(serialize = "do_deploy")]
     DoDeploy,
     /// Other tasks
+    #[strum(serialize = "do_fetch")]
     DoFetch,
     /// If it's a type of failure we're not familiar with or parsing fails, default to this
     #[default]
+    #[strum(serialize = "misc")]
     Misc,
 }
 
-impl Display for YoctoFailureKind {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let kind = match self {
-            YoctoFailureKind::DoBuild => "do_build",
-            YoctoFailureKind::DoCompile => "do_compile",
-            YoctoFailureKind::DoCompilePtestBase => "do_compile_ptest_base",
-            YoctoFailureKind::DoConfigure => "do_configure",
-            YoctoFailureKind::DoConfigurePtestBase => "do_configure_ptest_base",
-            YoctoFailureKind::DoDeploy => "do_deploy",
-            YoctoFailureKind::DoFetch => "do_fetch",
-            YoctoFailureKind::Misc => "misc",
-        };
-        write!(f, "{kind}")
-    }
-}
-
 impl YoctoFailureKind {
-    const TASK_NAMES: [&'static str; 7] = [
-        "do_build",
-        "do_compile",
-        "do_compile_ptest_base",
-        "do_configure",
-        "do_configure_ptest_base",
-        "do_deploy",
-        "do_fetch",
-    ];
-
-    pub fn from_task(task: &str) -> Self {
-        match task {
-            "do_build" => YoctoFailureKind::DoBuild,
-            "do_compile" => YoctoFailureKind::DoCompile,
-            "do_compile_ptest_base" => YoctoFailureKind::DoCompilePtestBase,
-            "do_configure" => YoctoFailureKind::DoConfigure,
-            "do_configure_ptest_base" => YoctoFailureKind::DoConfigurePtestBase,
-            "do_deploy" => YoctoFailureKind::DoDeploy,
-            "do_fetch" => YoctoFailureKind::DoFetch,
-            _ => YoctoFailureKind::Misc,
-        }
-    }
     /// Takes in a yocto logfile filename such as `log.do_fetch.21616` and attempts to determine the type
     /// of yocto task the the logfile is associated with.
     ///
+    /// # Example
+    /// ```
+    /// # use gh_workflow_parser::err_msg_parse::yocto_err::YoctoFailureKind;
+    /// let kind = YoctoFailureKind::parse_from_logfilename("log.do_fetch.21616").unwrap();
+    /// assert_eq!(kind, YoctoFailureKind::DoFetch);
     ///
+    /// // Infallible if you're sure the filename is a yocto log but it might not be a known task
+    /// let kind = YoctoFailureKind::parse_from_logfilename("log.some_custom_task.21616").unwrap_or_default();
+    /// assert_eq!(kind, YoctoFailureKind::Misc);
+    /// ```
     pub fn parse_from_logfilename(fname: &str) -> Result<Self, Box<dyn Error>> {
-        for name in Self::TASK_NAMES {
-            if fname.contains(name) {
-                return Ok(Self::from_task(name));
+        for variant in YoctoFailureKind::iter() {
+            let variant_as_str = variant.to_string();
+            if fname.contains(&variant_as_str) {
+                return Ok(variant);
             }
         }
         Err(format!("Could not determine task from input: {fname}").into())
     }
 }
 
-/// Find the `--- Error summary ---` section in the log and return the rest of the log until `bitbake -c build <string> failed with error 1`
-pub fn parse_yocto_error(log: &str) -> Result<YoctoError, Box<dyn Error>> {
+/// Find the `--- Error summary ---` section in the log and return the rest of the log.
+fn yocto_error_summary(log: &str) -> Result<String, Box<dyn Error>> {
     const YOCTO_ERROR_SUMMARY_SIGNATURE: &str = "--- Error summary ---";
     let error_summary = log
         .split(YOCTO_ERROR_SUMMARY_SIGNATURE)
         .collect::<Vec<&str>>()
         .pop()
         .ok_or("No error summary found")?;
-    // the prefix is like `Test template xilinx	📦 Build yocto image	2024-02-11T00:09:04.7119455Z`
-    // Get the prefix `Test template xilinx	📦 Build yocto image` (remove trailing whitespace)
+    Ok(error_summary.trim().to_string())
+}
 
-    // Trim the leading/trailing newlines and whitespace
-    let error_summary = error_summary.trim().to_string();
-    log::debug!("Yocto error before trimming: \n{}", error_summary);
-    // Trim trailing Just recipes like
-    // error: Recipe `in-container-build-ci-image` failed on line 31 with exit code 2
-    // error: Recipe `run-in-docker` failed with exit code 2 .. etc.
-    let error_summary = error_summary
+/// Trim the trailing `error: Recipe` lines from the error summary
+/// This is to remove the noise of just recipe failures
+fn trim_trailing_just_recipes(log: &str) -> Result<String, Box<dyn Error>> {
+    let trimmed = log
         .lines()
         .rev()
-        .skip(1) // Skip the last line, which is the github specific step failure line
         .skip_while(|line| line.starts_with("error: Recipe"))
-        .collect::<Vec<&str>>() // Then reverse the iterator to get the original order
+        .collect::<Vec<&str>>()
         .iter()
         .rev()
-        .fold(String::with_capacity(error_summary.len()), |acc, line| {
+        .fold(String::with_capacity(log.len()), |acc, line| {
             acc + line + "\n"
         });
+    Ok(trimmed)
+}
 
+/// Find the kind of yocto failure in the string e.g. this would be `do_fetch`
+/// ERROR: Logfile of failure stored in: /app/yocto/build/tmp/work/x86_64-linux/sqlite3-native/3.43.2/temp/log.do_fetch.21616
+///
+/// # Example
+/// ```
+/// use gh_workflow_parser::err_msg_parse::yocto_err::find_yocto_failure_log_str;
+/// let log = r#"ERROR: Some error message
+/// ERROR: Logfile of failure stored in: /app/yocto/build/tmp/work/x86_64-linux/sqlite3-native/3.43.2/temp/log.do_fetch.21616
+/// ERROR: Some other error message"#;
+///
+/// let failure_log_str = find_yocto_failure_log_str(log).unwrap();
+///
+/// assert_eq!(failure_log_str, "ERROR: Logfile of failure stored in: /app/yocto/build/tmp/work/x86_64-linux/sqlite3-native/3.43.2/temp/log.do_fetch.21616");
+/// ```
+///
+///
+pub fn find_yocto_failure_log_str(log: &str) -> Result<&str, Box<dyn Error>> {
+    let log_file_line = log
+        .lines()
+        .find(|line| line.contains("Logfile of failure stored in"))
+        .ok_or("No log file line found")?;
+
+    Ok(log_file_line)
+}
+
+/// Find the `--- Error summary ---` section in the log and return the rest of the log until `bitbake -c build <string> failed with error 1`
+pub fn parse_yocto_error(log: &str) -> Result<YoctoError, Box<dyn Error>> {
+    let error_summary = yocto_error_summary(log)?;
+    log::debug!(
+        "Yocto error before trimming just recipe failures: \n{}",
+        error_summary
+    );
+
+    let error_summary = trim_trailing_just_recipes(&error_summary)?;
     log::info!("Yocto error: \n{}", error_summary);
 
     // Find the kind of yocto failure in the string e.g. this would be `do_fetch`
     // ERROR: Logfile of failure stored in: /app/yocto/build/tmp/work/x86_64-linux/sqlite3-native/3.43.2/temp/log.do_fetch.21616
 
     // Find the line with the `Logfile of failure stored in` and get the path
-    let log_file_line = error_summary
-        .lines()
-        .find(|line| line.contains("Logfile of failure stored in"))
-        .ok_or("No log file line found")?;
+    let log_file_line = find_yocto_failure_log_str(&error_summary)?;
     let path = first_abs_path_from_str(log_file_line)?;
     let fname = path.file_stem().unwrap().to_str().unwrap();
     let yocto_failure_kind = match YoctoFailureKind::parse_from_logfilename(fname) {
@@ -183,6 +195,7 @@ pub fn parse_yocto_error(log: &str) -> Result<YoctoError, Box<dyn Error>> {
 mod tests {
     use super::*;
     use pretty_assertions::assert_eq;
+    use std::str::FromStr;
 
     const ERROR_SUMMARY_TEST_STR: &str = r#"ERROR: sqlite3-native-3_3.43.2-r0 do_fetch: Bitbake Fetcher Error: MalformedUrl('${SOURCE_MIRROR_URL}')
     ERROR: Logfile of failure stored in: /app/yocto/build/tmp/work/x86_64-linux/sqlite3-native/3.43.2/temp/log.do_fetch.21616
@@ -193,7 +206,10 @@ mod tests {
     #[test]
     fn test_determine_yocto_error_kind() {
         let task = "do_build";
-        assert_eq!(YoctoFailureKind::from_task(task), YoctoFailureKind::DoBuild);
+        assert_eq!(
+            YoctoFailureKind::from_str(task).unwrap(),
+            YoctoFailureKind::DoBuild
+        );
     }
 
     #[test]
